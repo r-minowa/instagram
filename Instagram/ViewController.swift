b2020bebfea4f38c7fee0c1626bdf3918bdd61a2
r-minowa <r-minowa@ienter.co.jp>
2020-04-23 11:11:20 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by 蓑輪 竜輝 on 2020/04/23.
+//  Copyright © 2020 ryuki.minowa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
